4958c1f642ed6a64473e998aa8970f095a7f563e
Documenter.jl <documenter@juliadocs.github.io>
2021-08-06 07:45:51 +0000

    build based on f3b16c5

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"index.html#NASAPrecipitation","page":"Home","title":"NASAPrecipitation","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Dealing with Datasets from the NASA Precipitation Measurement Mission","category":"page"},{"location":"index.html","page":"Home","title":"Home","text":"Documentation for NASAPrecipitation.jl.","category":"page"}]
+[{"location":"index.html#[NASAPrecipitation.jl](https://github.com/natgeo-wong/NASAPrecipitation.jl)","page":"Home","title":"NASAPrecipitation.jl","text":"","category":"section"},{"location":"index.html","page":"Home","title":"Home","text":"Dealing with Datasets from the NASA Precipitation Measurement Mission","category":"page"}]
 }
