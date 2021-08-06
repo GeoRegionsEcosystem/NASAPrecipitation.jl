ed18911064db937b63fb2752ad9e04dd70221344
Nathanael Wong <natgeo.wong@outlook.com>
2021-08-06 03:22:49 -0400

    Test documentation capabilities

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,18 @@
 using NASAPrecipitation
 using Documenter
 
-DocMeta.setdocmeta!(NASAPrecipitation, :DocTestSetup, :(using NASAPrecipitation); recursive=true)
-
 makedocs(;
-    modules=[NASAPrecipitation],
-    authors="Nathanael Wong <natgeo.wong@outlook.com>",
-    repo="https://github.com/natgeo-wong/NASAPrecipitation.jl/blob/{commit}{path}#{line}",
-    sitename="NASAPrecipitation.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        assets=String[],
-    ),
-    pages=[
+    modules  = [NASAPrecipitation],
+    doctest  = false,
+    format   = Documenter.HTML(collapselevel=1,prettyurls=false),
+    authors  = "Nathanael Wong <natgeo.wong@outlook.com>",
+    sitename = "NASAPrecipitation.jl",
+    pages    = [
         "Home" => "index.md",
     ],
+)
+
+deploydocs(
+    repo = "github.com/natgeo-wong/NASAPrecipitation.jl.git",
+    devbranch = "main"
 )
